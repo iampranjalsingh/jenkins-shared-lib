45a520368793b3c9c42a31666439bb71f5952ba1
Pranjal Singh <88673276+iampranjalsingh@users.noreply.github.com>
2023-03-15 23:13:50 +0530

    Update helloWorld.groovy

--- vars/helloWorld.groovy
+++ vars/helloWorld.groovy
@@ -1,6 +1,5 @@
 #!/usr/bin/env groovy
 
 def call(Map config = [:]) {
-    bat "echo Hello ${config.name}. Today is ${config.dayOfWeek}."
+    sh "echo Hello ${config.name}. Today is ${config.dayOfWeek}."
 }
-return this
