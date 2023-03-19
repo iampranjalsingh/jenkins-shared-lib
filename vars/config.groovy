0b5b7454a29cf2fe1f14f55f980b332345491d12
Pranjal Singh <iampranjalsingh@gmail.com>
2023-03-20 02:10:44 +0530

    Commit

--- vars/config.groovy
+++ vars/config.groovy
@@ -1,22 +1,28 @@
 #!/usr/bin/env groovy
+import com.localhost.swb.IniParser
 
 def call(String filePath, Closure closure) {
-    def config = libraryResource 'globalConfig.ini'
-    println "$config"
-    /*def map = readProperties text: config
-    println "$map"*/
-    println config.getClass()
-
-    if (!filePath.isEmpty()) {
+    readConfig()
+    /*if (!filePath.isEmpty()) {
         def file_exist = findFiles (glob: filePath)
         if (filePath.length == 1) {
             def  map1 = readYaml file: filePath
             println map1
             map.putAll(map1)
         }
-    }
+    }*/
 }
 
 def call(Closure closure) {
     call('', closure)
+}
+
+
+def readConfig() {
+    def ini = new IniParser("configJenkins.ini")
+    ini.dumpConfig()
+    def secs = ini.getAllSections()
+    secs.each() { it ->
+        println it
+    }
 }
