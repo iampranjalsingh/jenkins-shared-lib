e60f0eb636c2012460f83fe56471d6b979e5169f
Pranjal Singh <iampranjalsingh@gmail.com>
2023-03-20 05:08:51 +0530

    Commit

--- vars/configSetup.groovy
+++ vars/configSetup.groovy
@@ -1,5 +1,6 @@
 #!/usr/bin/env groovy
 import com.localhost.swb.IniParser
+import com.cloudbees.groovy.cps.NonCPS
 
 def call(Map config = [:]) {
     if (!config.isEmpty() && config.containsKey("configFile")) {
@@ -8,8 +9,12 @@ def call(Map config = [:]) {
      def configContents = libraryResource "globalConfig.ini"
      println "${configContents}"
      writeFile file: "GLOBAL_CONFIG.ini", text: "${configContents}"
+    readConfig("${env.WORKSPACE}/GLOBAL_CONFIG.ini")
+}
 
-    def ini = new IniParser("${env.WORKSPACE}/GLOBAL_CONFIG.ini")
+@NonCPS
+def readConfig(String fileToRead) {
+    def ini = new IniParser("${fileToRead}")
     ini.dumpConfig()
     def secs = ini.getAllSections()
     secs.each() { it ->
