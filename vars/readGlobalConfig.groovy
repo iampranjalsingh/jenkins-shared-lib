abc5485aa246cf5f761df556162c825d01199697
Pranjal Singh <iampranjalsingh@gmail.com>
2023-03-20 06:14:11 +0530

    Commit

--- vars/readGlobalConfig.groovy
+++ vars/readGlobalConfig.groovy
@@ -4,11 +4,12 @@ def call(String filePath="configJenkins.ini") {
     def fileContents = libraryResource 'globalConfig.ini'
     def props = readProperties(text:fileContents)
     if (!filePath.isEmpty()) {
-        def file_exist = findFiles (glob: filePath)
-        if (filePath.length == 1) {
-            def  map = readYaml file: filePath
+        def fileExist = findFiles (glob: filePath)
+        println "${fileExist}"
+        /*if (fileExist == 1) {
+            def  map = readProperties file: filePath
             props.putAll(map)
-        }
+        }*/
     }
     return props
 }
