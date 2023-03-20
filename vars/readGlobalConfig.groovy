7283b671fa151b59671cd0339fa8e759e7e4c11d
Pranjal Singh <iampranjalsingh@gmail.com>
2023-03-20 05:34:51 +0530

    Commit

--- vars/readGlobalConfig.groovy
+++ vars/readGlobalConfig.groovy
@@ -1,10 +1,8 @@
 #!/usr/bin/env groovy
-import com.localhost.swb.IniParser
-import com.cloudbees.groovy.cps.NonCPS
 
 def call(Map config = [:]) {
     def conf = readProperties file: "configJenkins.ini"
-    println "${config}"
+    println "${conf}"
     /*def configContents = libraryResource "globalConfig.ini"
     writeFile file: "GLOBAL_CONFIG.ini", text: "${configContents}"
     readConfig("${env.WORKSPACE}/GLOBAL_CONFIG.ini")*/
