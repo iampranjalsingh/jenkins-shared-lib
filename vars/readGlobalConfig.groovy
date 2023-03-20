ce48989f0328da5ba33681ac9ad0f4431e36751c
Pranjal Singh <iampranjalsingh@gmail.com>
2023-03-20 05:50:42 +0530

    Commit

--- vars/readGlobalConfig.groovy
+++ vars/readGlobalConfig.groovy
@@ -1,13 +1,9 @@
 #!/usr/bin/env groovy
 
 def call(Map config = [:] as Map) {
-
-    def request = libraryResource 'configJenkins.ini'
-    println "$request"
-
-    def properties = readProperties file:"configJenkins.ini"
-    println "${conf}"
-    /*def configContents = libraryResource "globalConfig.ini"
-    writeFile file: "GLOBAL_CONFIG.ini", text: "${configContents}"
-    readConfig("${env.WORKSPACE}/GLOBAL_CONFIG.ini")*/
+    def fileContents = libraryResource 'configJenkins.ini'
+    def props = readProperties(text:fileContents)
+    println(fileContents)
+    println("**********************")
+    println(props)
 }
