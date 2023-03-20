d45f3cdb90ec0a7f44d26d820eabf8632917ec22
Pranjal Singh <iampranjalsingh@gmail.com>
2023-03-20 05:53:17 +0530

    Commit

--- vars/readGlobalConfig.groovy
+++ vars/readGlobalConfig.groovy
@@ -4,6 +4,6 @@ def call(Map config = [:] as Map) {
     def fileContents = libraryResource 'configJenkins.ini'
     def props = readProperties(text:fileContents)
     println(fileContents)
-    println("**************** ${config['template']}")
+    println("**************** ${props['template']}")
     println(props)
 }
